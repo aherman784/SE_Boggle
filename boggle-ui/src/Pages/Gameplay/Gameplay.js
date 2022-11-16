f74eddf46a60965df4e2624bbdceb85c85204752
Logan White <logan.white225@gmail.com>
2022-11-16 16:50:59 -0600

    Added word bank and word search UI

--- boggle-ui/src/Pages/Gameplay/Gameplay.js
+++ boggle-ui/src/Pages/Gameplay/Gameplay.js
@@ -6,15 +6,44 @@ export function Gameplay() {
         ['A', 'B', 'C', 'D'],
         ['E', 'F', 'G', 'H'],
         ['I', 'J', 'K', 'L'],
-        ['M', 'N', 'O', 'P']
+        ['M', 'N', 'O', 'P'],
     ]);
     const [players, setPlayers] = useState([
-        {Username: 'Logan', Score: 11},
-        {Username: 'Hunter', Score: 17},
-        {Username: 'Matthew', Score: 8},
-        {Username: 'Alex', Score: 14},
-
+        {Id: 1, Username: 'Logan', Score: 11},
+        {Id: 2, Username: 'Hunter', Score: 17},
+        {Id: 3, Username: 'Matthew', Score: 8},
+        {Id: 4, Username: 'Alex', Score: 14},
     ]);
+    const [wordsGuessed, setWordsGuessed] = useState([
+        'aa',
+        'aaa',
+        'aachen',
+        'aardvark',
+        'aardvarks',
+        'aaron',
+        'ab',
+        'aba',
+        'ababa',
+        'abaci',
+        'aback',
+        'abactor',
+        'abactors',
+        'abacus',
+        'abacuses',
+        'abaft',
+        'abalone',
+        'abandon',
+        'abandoned',
+        'abandonee',
+        'abandonees',
+        'abandoning',
+        'abandonment',
+        'abandons',
+        'abase',
+        'abased',
+        'abasement',
+        
+    ])
     return(
         <div className="gameplay-container">
             <div className='grid-and-leaderboard'>
@@ -37,7 +66,18 @@ export function Gameplay() {
                 </div>
             </div>
             <div className='word-bank'>
-                
+                <div className='word-bank-header'>
+                    <div className='word-guess-search'>
+                        <input className='word-guess-input'></input>
+                        <button className='word-guess-enter'>Enter</button>
+                    </div>
+                    <div className='player-score'>Score: 11</div>
+                </div> 
+                <div className='word-bank-words'>
+                    {wordsGuessed.map(word => (
+                        <div className='word-bank-word'>{word}</div>
+                    ))}
+                </div>
             </div>
         </div>
     );
