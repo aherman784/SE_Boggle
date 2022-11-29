d542af0917aeee07999466bdcab692c18fba6a9e
Logan White <logan.white225@gmail.com>
2022-11-29 10:28:46 -0600

    Added tests for word validity engine

--- BoggleAPI/BoggleAPITest/SourceTest/EngineTest/EngineTest.cs
+++ BoggleAPI/BoggleAPITest/SourceTest/EngineTest/EngineTest.cs
@@ -1,3 +1,5 @@
+using BoggleAPI.Source.Engine;
+
 namespace BoggleAPITest
 {
     public class EngineTest
@@ -8,9 +10,73 @@ namespace BoggleAPITest
         }
 
         [Test]
-        public void Test1()
+        public void IsWordCorrectLength1()
         {
-            Assert.Pass();
+            // Arrange
+            var WordEngine = new WordValidityEngine();
+            bool expected = true;
+
+            //Act
+            bool actual = WordEngine.IsWordCorrectLength("apple");
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void IsWordCorrectLength2()
+        {
+            // Arrange
+            var WordEngine = new WordValidityEngine();
+            bool expected = false;
+
+            //Act
+            bool actual = WordEngine.IsWordCorrectLength("a");
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void IsWordCorrectLength3()
+        {
+            // Arrange
+            var WordEngine = new WordValidityEngine();
+            bool expected = true;
+
+            //Act
+            bool actual = WordEngine.IsWordCorrectLength("averylongstringg");
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void IsWordCorrectLength4()
+        {
+            // Arrange
+            var WordEngine = new WordValidityEngine();
+            bool expected = false;
+
+            //Act
+            bool actual = WordEngine.IsWordCorrectLength("averylongstringgg");
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void IsWordCorrectLength5()
+        {
+            // Arrange
+            var WordEngine = new WordValidityEngine();
+            bool expected = true;
+
+            //Act
+            bool actual = WordEngine.IsWordCorrectLength("university");
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
         }
     }
 }
