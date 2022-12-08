faba3666251cffe78e373e33111c88fc871da58e
Matthew Rokusek <matthewrokusek4747@gmail.com>
2022-12-08 15:20:18 -0600

    removed unnecissary file

--- BoggleAPI/BoggleAPI/app.cs
+++
@@ -1,6 +0,0 @@
-﻿<?xml version="1.0" encoding="utf-8"?>
-<configuration>
-	<connectionStrings>
-		<add name="connString" connectionString="server=167.248.80.128;uid=###;pwd=###;database=sys;" />
-	</connectionStrings>
-</configuration>
